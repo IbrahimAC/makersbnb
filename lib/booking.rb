5f074936a6c6ff944aa77d05ad44b2bf1cacef6a
Tomas Garcia <tomasgarciadev@gmail.com>
2021-12-14 16:16:46 +0000

    class booking and request method

---
+++ lib/booking.rb
@@ -0,0 +1,19 @@
+class Booking
+  attr_reader :id, :user_id, :space_id, :date, :confirmed
+
+  def initialize(id, user_id, space_id, date, confirmed)
+    @id = id
+    @user_id = user_id
+    @space_id = space_id
+    @date = date
+    @confirmed = confirmed
+  end
+  def self.request(user_id, space_id, date)
+    result = DatabaseConnection.query(
+      "INSERT INTO bookings (user_id, space_id, date) VALUES($1, $2, $3) 
+      RETURNING id, user_id, space_id, date, confirmed;", 
+      [user_id, space_id, date]
+      )
+    Booking.new(result[0]['id'], result[0]['user_id'], result[0]['space_id'], result[0]['date'], result[0]['confirmed'])
+  end
+end
