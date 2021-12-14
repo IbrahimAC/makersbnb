a886e5070b5d5073b4a30d3babf3990930c5a618
Tomas Garcia <tomasgarciadev@gmail.com>
2021-12-14 17:05:40 +0000

    Users can confirm or deny booking (Booking class)

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -2,8 +2,6 @@ require 'booking'
 
 describe Booking do
   it 'should be able to request a new booking' do
-    DatabaseConnection.query('ALTER SEQUENCE users_id_seq RESTART WITH 1;')
-    DatabaseConnection.query('ALTER SEQUENCE spaces_id_seq RESTART WITH 1;')
     user = User.create('Tomas', 'tomas_fake_email@gmail.com', 'password123')
     space = Space.create(title: 'House', description: 'My house', picture: 'url', price: 120, user_id: user.id)
 
@@ -13,5 +11,23 @@ describe Booking do
     expect(booking.space_id).to eq space.id
     expect(booking.date).to eq '2022-01-08'
     expect(booking.confirmed).to be_nil
+  end
+
+  it 'should be able to confirm a booking' do
+    user = User.create('Tomas', 'tomas_fake_email@gmail.com', 'password123')
+    space = Space.create(title: 'House', description: 'My house', picture: 'url', price: 120, user_id: user.id)
+    booking = Booking.request(user.id, space.id, '2022-01-08')
+
+    confirmed_booking = Booking.confirm(booking.id, true)
+    expect(confirmed_booking.confirmed).to eq "t"
+  end
+
+  it 'should be able to reject a booking' do
+    user = User.create('Tomas', 'tomas_fake_email@gmail.com', 'password123')
+    space = Space.create(title: 'House', description: 'My house', picture: 'url', price: 120, user_id: user.id)
+    booking = Booking.request(user.id, space.id, '2022-01-08')
+
+    rejected_booking = Booking.confirm(booking.id, false)
+    expect(rejected_booking.confirmed).to eq "f"
   end
 end
