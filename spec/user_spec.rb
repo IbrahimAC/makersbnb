8e11932c79acf68f5abcd8e25641c56b396d6455
IbrahimAC <ibrahimchowdhury.notwork@gmail.com>
2021-12-14 09:45:55 +0000

    rubocop

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require 'user'
 
 describe User do
-
-  it "creates a new user" do
-    user = User.create(name: "Tomas", email: "tomas_fake_email@gmail.com", password: "password123")
+  it 'creates a new user' do
+    user = User.create(name: 'Tomas', email: 'tomas_fake_email@gmail.com', password: 'password123')
     expect(user).to be_a_kind_of User
-    expect(user.name).to eq "Tomas"
-    expect(user.email).to eq "tomas_fake_email@gmail.com"
+    expect(user.name).to eq 'Tomas'
+    expect(user.email).to eq 'tomas_fake_email@gmail.com'
   end
-
 end
