c2440dbe71a6df78eb4fd3117c3f569774d4b3ab
Tomas Garcia <tomasgarciadev@gmail.com>
2021-12-15 16:11:34 +0000

    implemented date columns in space/new

--- lib/space.rb
+++ lib/space.rb
@@ -3,21 +3,23 @@
 require_relative 'database_connection'
 
 class Space
-  attr_reader :id, :title, :description, :picture, :price, :user_id
+  attr_reader :id, :title, :description, :picture, :price, :user_id, :availability_from, :availability_until
 
-  def initialize(id:, title:, description:, picture:, price:, user_id:)
+  def initialize(id:, title:, description:, picture:, price:, user_id:, availability_from:, availability_until:)
     @id = id
     @title = title
     @description = description
     @picture = picture
     @price = price
     @user_id = user_id
+    @availability_from = availability_from
+    @availability_until = availability_until
   end
 
-  def self.create(title:, description:, picture:, price:, user_id:)
-    res = DatabaseConnection.query("INSERT INTO spaces(title,description,picture,price,user_id)
-    VALUES($1,$2,$3,$4,$5) RETURNING id,title,description,picture,price,user_id;",
-                                   [title, description, picture, price, user_id])
+  def self.create(title:, description:, picture:, price:, user_id:, availability_from:, availability_until:)
+    res = DatabaseConnection.query("INSERT INTO spaces(title, description, picture, price, user_id, availability_from, availability_until)
+    VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING id, title, description, picture, price, user_id, availability_from, availability_until;",
+                                   [title, description, picture, price, user_id, availability_from, availability_until])
 
     Space.new(
       id: res[0]['id'],
@@ -25,7 +27,9 @@ class Space
       description: res[0]['description'],
       picture: res[0]['picture'],
       price: res[0]['price'],
-      user_id: res[0]['user_id']
+      user_id: res[0]['user_id'],
+      availability_from: res[0]['availability_from'],
+      availability_until: res[0]['availability_until']
     )
   end
 
@@ -38,21 +42,24 @@ class Space
         description: space['description'],
         picture: space['picture'],
         price: space['price'],
-        user_id: space['user_id']
+        user_id: space['user_id'],
+        availability_from: space['availability_from'],
+        availability_until: space['availability_until']
       )
     end
   end
 
   def self.find(id:)
-      res = DatabaseConnection.query("SELECT * FROM spaces WHERE id = $1", [id])
-      Space.new(
-        id: res[0]['id'],
-        title: res[0]['title'],
-        description: res[0]['description'],
-        picture: res[0]['picture'],
-        price: res[0]['price'],
-        user_id: res[0]['user_id']
-      )
+    res = DatabaseConnection.query('SELECT * FROM spaces WHERE id = $1', [id])
+    Space.new(
+      id: res[0]['id'],
+      title: res[0]['title'],
+      description: res[0]['description'],
+      picture: res[0]['picture'],
+      price: res[0]['price'],
+      user_id: res[0]['user_id'],
+      availability_from: res[0]['availability_from'],
+      availability_until: res[0]['availability_until']
+    )
   end
-
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -40,5 +40,4 @@ describe 'Space' do
 
     expect(found_space.id).to eq space.id
   end
-
 end
