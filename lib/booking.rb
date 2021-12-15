1afbf28073969ed7cfc609d4ef12df34fb671aa8
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-12-15 11:05:39 +0000

    Progress on making bookings

--- lib/booking.rb
+++ lib/booking.rb
@@ -10,6 +10,7 @@ class Booking
   end
 
   def self.request(user_id, space_id, date)
+    return nil if Booking.already_booked?(space_id, date)
     result = DatabaseConnection.query(
       "INSERT INTO bookings (user_id, space_id, date) VALUES($1, $2, $3) 
       RETURNING id, user_id, space_id, date, confirmed;", 
@@ -22,4 +23,15 @@ class Booking
     result = DatabaseConnection.query("UPDATE bookings SET confirmed = $2 WHERE id = $1 RETURNING id, user_id, space_id, date, confirmed;", [id, decision])
     Booking.new(result[0]['id'], result[0]['user_id'], result[0]['space_id'], result[0]['date'], result[0]['confirmed'])
   end
+
+  def self.already_booked?(space_id, date)
+    result = DatabaseConnection.query("select exists(select 1 from bookings where space_id=$1 AND date=$2 AND confirmed='t');", [space_id, date])
+    result[0]['exists'] == 't'
+  end
+
+  def self.unavailable_dates(space_id)
+    result = DatabaseConnection.query("SELECT * FROM bookings where space_id=$1 AND confirmed='t';", [space_id])
+    result.map {|booking| booking['date']}
+  end
+
 end
---
+++ spec/features/request_booking_spec.rb
@@ -0,0 +1,25 @@
+feature 'requesting booking' do
+  scenario 'user should be able to request booking' do
+    sign_up
+    visit '/spaces/new'
+    fill_in 'title', with: 'Test house'
+    fill_in 'description', with: 'house description'
+    fill_in 'price', with: '25'
+    fill_in 'picture', with: 'url'
+    click_button 'Create space'
+    visit('/')
+    click_button 'Log out'
+    visit('/')
+    click_button('Sign up')
+    fill_in('name', with: 'Kim')
+    fill_in('email', with: 'kim_fake_email@gmail.com')
+    fill_in('password', with: 'password123')
+    click_button('Submit')
+    visit('/spaces')
+    click_link('Test house')
+    click_button('Request booking')
+    fill_in('date', with: '2022-01-05')
+    click_button('Submit')
+    expect(page).to have_content 'Request made'
+  end
+end
